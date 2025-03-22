13b2592efe8fd4033c09a1dbeedc7b12a73d800b
adriano-ms <amaciel_sanchez@hotmail.com>
2025-03-22 11:22:39 -0300

    Aluno Entity annotations

--- src/main/java/com/example/carometro/aluno/Aluno.java
+++ src/main/java/com/example/carometro/aluno/Aluno.java
@@ -1,8 +1,20 @@
 package com.example.carometro.aluno;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Entity
+@Table(name = "aluno")
+@Entity (name = "aluno")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Aluno {
 	private long id;
 	private String matricula;
